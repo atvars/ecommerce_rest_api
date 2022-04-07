2f5b449b6984448de3d9f28c4b5ebd3f97ae37b7
atvars <avelavs@gmail.com>
2022-04-07 23:13:35 +0100

    added routes folder with router files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models.js');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+// set up cors
+app.use(cors());
+// set up that our app can parse .json format
+app.use(express.json());
+
+// checking if server is working
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Working!' });
+// });
 
 const start = async () => {
   try {
