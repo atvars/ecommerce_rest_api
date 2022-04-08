0d7cb944803fd848fc89cff657b761271f4dce9b
atvars <avelavs@gmail.com>
2022-04-08 23:23:09 +0100

    created controllers for the routes

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,11 @@
+class DeviceController {
+    async create(req, res) {}
+  
+    async getAll(req, res) {}
+
+    async getOne(req, res) {}
+
+  }
+  
+  module.exports = new DeviceController();
+  
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,14 @@
 const Router = require('express');
 const router = new Router();
+const userController = require('../controllers/userController')
 
-router.post('/registration',);
-router.post('/login',);
-router.get('/auth',)
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.get('/auth', userController.check);
 
 // checked rout to server
 // router.get('/auth', (req, res) => {
 //     res.json({message: 'All Working'})
 // });
-
-
 
 module.exports = router;
