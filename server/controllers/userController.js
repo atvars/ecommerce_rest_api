1fa820bb224139c0a3ed34d8167e9154aa1779eb
atvars <avelavs@gmail.com>
2022-04-13 13:46:37 +0100

    done with user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,50 @@
 const ApiError = require('../error/ApiError');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { User, Basket } = require('../models/models');
 
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id: user.id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: '24h',
+  });
+};
+
 class UserController {
+  // registration
   async registration(req, res) {
-    
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest('Wrong email or password'));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest('User with this email already exists'));
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  // Login
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal('User not found!'));
+    }
+    // to check if password match the one in the database
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('You entered wrong password!'));
+    }
+    const token = generateJWT(user.id, user.email, user.role);
+    return res.json({ token });
   }
 
-  async login(req, res) {}
-
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest('Need to add ID'));
-    }
-    res.json(id);
+    const token = generateJwt(req.user.id, req.user.email, req.user.role)
+    return res.json({token})
   }
 }
 
